426858200266115aeea0069e86e92056e26e1b35
jakdalmak7961 <jakdalmak7961@gmail.com>
2026-09-07 06:34:35 +0900

    chore: setting server default data

---
+++ src/main/java/kr/co/teambrain/marvel_run/MarvelRunApplication.java
@@ -0,0 +1,13 @@
+package kr.co.teambrain.marvel_run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelRunApplication.class, args);
+	}
+
+}
