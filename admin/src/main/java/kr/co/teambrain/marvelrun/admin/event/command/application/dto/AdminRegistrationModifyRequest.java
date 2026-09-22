baa960d574b4091c3eb2974325011b47664a0d13
JungUiJin <yealkki38@gmail.com>
2026-09-22 12:36:30 +0900

    Fix: 정보 수정시 제약조건 추가

--- admin/src/main/java/kr/co/teambrain/marvelrun/admin/event/command/application/dto/AdminRegistrationModifyRequest.java
+++ admin/src/main/java/kr/co/teambrain/marvelrun/admin/event/command/application/dto/AdminRegistrationModifyRequest.java
@@ -2,6 +2,7 @@ package kr.co.teambrain.marvelrun.admin.event.command.application.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import kr.co.teambrain.marvelrun.common.inheritance_enum.GenderClass;
 
 /**
@@ -12,9 +13,11 @@ public record AdminRegistrationModifyRequest(
         String name,
 
         @NotBlank(message = "연락처를 입력해주세요.")
+        @Pattern(regexp = "^[0-9]+$", message = "연락처는 '-' 없이 숫자만 입력해주세요.")
         String phNum,
 
         @NotBlank(message = "생년월일을 입력해주세요.")
+        @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "생년월일은 'YYYY-MM-DD' 형식(예: 2000-11-11)으로 입력해주세요.")
         String birth,
 
         @NotNull(message = "성별을 선택해주세요.")
@@ -22,8 +25,12 @@ public record AdminRegistrationModifyRequest(
 
         String address,
         String addressDetail,
+
         String guardianName,
+
+        @Pattern(regexp = "^[0-9]*$", message = "보호자 연락처는 '-' 없이 숫자만 입력해주세요.")
         String guardianPhNum,
+
         String guardianRelationship
 ) {
 }
