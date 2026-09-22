cad3b6904c1d2d44a349e1261e3ce2f86ce8952c
JungUiJin <yealkki38@gmail.com>
2026-09-22 17:12:40 +0900

    feat: repository query alias 추가

--- user/src/main/java/kr/co/teambrain/marvelrun/user/event/query/repository/RegistrationQueryRepository.java
+++ user/src/main/java/kr/co/teambrain/marvelrun/user/event/query/repository/RegistrationQueryRepository.java
@@ -25,7 +25,7 @@ public class RegistrationQueryRepository {
     private final EntityManager entityManager;
 
     private static final String MEMBER_SELECT = """
-            select r.id as id, r.name as name, r.email, r.birth as birth, r.phNum as phNum,
+            select r.id as id, r.name as name, r.email as email, r.birth as birth, r.phNum as phNum,
                    r.password as password, r.gender as gender,
                    c.id as categoryId, c.name as categoryName, r.souvenirJson as souvenirs,
                    r.address as address, r.addressDetail as addressDetail,
