2ec5f7281b9c477353f67b0b9bee7b1e78dcede5
jakdalmak7961 <jakdalmak7961@gmail.com>
2026-09-24 06:27:16 +0900

    chore : 임포트누락수정

--- admin/src/main/java/kr/co/teambrain/marvelrun/admin/common/exception/GlobalExceptionHandler.java
+++ admin/src/main/java/kr/co/teambrain/marvelrun/admin/common/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import kr.co.teambrain.marvelrun.admin.auth.command.application.exception.AuthEr
 import kr.co.teambrain.marvelrun.admin.auth.command.application.exception.JwtAuthenticationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
